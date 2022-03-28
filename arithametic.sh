e515fb2dab81df96fbf1f8e14db51bbec5ee2ee1
git <git@gmail.com>
2022-03-28 09:03:25 +0000

    filesadded

---
+++ arithametic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#add 2 numbers
+echo "enter the 2 numbers"
+read a
+read b
+add=$((a + b))
+echo "sum of numbers" $add
+test=10
+echo "$test"
+echo 'test'
+a=11
+echo "'$a'"
+echo '"$a"'
+
